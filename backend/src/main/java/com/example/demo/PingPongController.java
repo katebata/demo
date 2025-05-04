f5cd056191e9afdbf8de803e3762165f08d029b9
Karim <karim.atebata@etu.univ-amu.fr>
2025-05-04 11:18:16 +0200

    CD workflow with new controller

---
+++ backend/src/main/java/com/example/demo/PingPongController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class PingPongController {
+
+    record PingPong(String message) {}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+}
