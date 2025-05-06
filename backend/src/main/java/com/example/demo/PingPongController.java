a0ceb51ae9e3ac95528812446d2363966a92a718
Karim <karim.atebata@etu.univ-amu.fr>
2025-05-06 08:02:51 +0200

    corrected  PingPongController

--- backend/src/main/java/com/example/demo/PingPongController.java
+++ backend/src/main/java/com/example/demo/PingPongController.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerAdvice
+@RestController
 public class PingPongController {
 
     record PingPong(String message) {}
